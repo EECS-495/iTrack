82e5cdaa04b68fcc8e8d3e8d979822cc51da2c4e
mananm12 <mananm@umich.edu>
2023-02-14 18:15:22 -0500

    deleted folder

--- iTrackAlpha/iTrackAlpha/ViewModel.swift
+++ iTrackAlpha/iTrackAlpha/ViewModel.swift
@@ -2,41 +2,7 @@
 //  ViewModel.swift
 //  iTrackAlpha
 //
-//  Created by Danielle Maraffino on 2/13/23.
+//  Created by Manan Manchanda on 2/14/23.
 //
 
 import Foundation
-import SwiftUI
-
-enum ActionType {
-    case blink, up, down, left, right, NONE
-}
-
-struct Action {
-    var actionType: ActionType
-}
-
-class ViewModel: ObservableObject {
-    @Published var value: Int = 0
-    var queue = [Action]()
-    
-    func pop() -> Action {
-        if !queue.isEmpty {
-            return queue.removeFirst()
-        }
-        return Action(actionType: ActionType.NONE)
-    }
-    
-    func push(action: Action){
-        queue.append(action)
-        DispatchQueue.main.asyncAfter(deadline: .now()) {
-            self.value = (self.value + 1) % 2
-        }
-    }
-    
-    func addBlink() {
-        DispatchQueue.main.asyncAfter(deadline: .now()) {
-            self.value = (self.value + 1) % 2
-        }
-    }
-}
